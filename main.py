7b25b0e330729ec9c46cc78af34beeee5244118e
Juan Perez <jperezllorente@hotmail.com>
2020-12-15 20:32:59 +0100

    final-project-07-folium

--- main.py
+++ main.py
@@ -4,26 +4,53 @@ from flask import Flask, request, render_template, jsonify
 import markdown.extensions.fenced_code
 import json
 import src.home as hm
-from wtforms import Form, FloatField, validators
+import src.maps as mp
+import folium
+from folium import Choropleth, Circle, Marker, Icon, Map
+
 
 app = Flask(__name__)
 
 
 @app.route("/form")
 def form():
-    cat_list = ["Transport", "Nightlife", "Gyms", "Hospital", "Entertainment", "Supermarkets", "Restaurants",
-                "Parks", "Pharmacy", "School", "Clothing store", ""]
-    return render_template('test.html', cat_list=cat_list)
+    cat_list = ["Transport", "Restaurants and Nightlife", "Gyms", "Hospital", "Entertainment", "Supermarkets",
+                "Parks", "Pharmacy", "School"]
+    dist_list = ['Moncloa','Centro','Tetuán','Chamartín','Salamanca','Retiro','San Blas',
+                 'Hortaleza','Chamberí','Fuencarral','Ciudad Lineal','Carabanchel','Arganzuela','Latina']
+    type_list = ["flat", "penthouse", "duplex"]
+
+    return render_template('test.html', cat_list=cat_list, dist_list=dist_list, type_list=type_list)
 
 
 @app.route("/input", methods=["GET"])
 def options():
+    dist = request.args.get("district")
     cat_1 = request.args.get("category_1")
     cat_2 = request.args.get("category_2")
     cat_3 = request.args.get("category_3")
-    result = hm.final(cat_1, cat_2, cat_3)
-    result.to_csv("data\\end_1.csv")
-    return result.to_html()
+    proptype = request.args.get("type")
+
+    result = hm.final(dist, cat_1, cat_2, cat_3, proptype)
+
+    start_coords = (40.437863, -3.690433)
+    final_map = folium.Map(location=start_coords, zoom_start=14)
+
+    for i, row in result.iterrows():
+        home = {
+            "location": [row["latitude"], row["longitude"]],
+            "tooltip": [row["price"], row["propertyType"], row["size"]]
+        }
+        icon = Icon(color="blue",
+                    prefix="fa",
+                    icon="home",
+                    icon_color="white")
+
+        Marker(**home, icon=icon).add_to(final_map)
+
+    return final_map._repr_html_()
+
+
 
 
 @app.route("/prueba", methods=["GET"])
--- src/home.py
+++ src/home.py
@@ -5,134 +5,110 @@ from pymongo import MongoClient, GEOSPHERE
 from configuration.config import homes, places
 from src.frame import geo_frame
 
-
-data = pd.read_csv("C:\\Users\\juanp\\Ironhack\\proyectos\\final-project\\data\\test.csv")
+data = pd.read_csv("C:\\Users\\juanp\\Ironhack\\proyectos\\final-project\\data\\vivienda.csv")
 data = geo_frame(data)
-test = data.head(100)
+test = data.sample(n=700)
 coordinates = [list(i["coordinates"]) for i in test.geometry]
 coord_prueba = coordinates[0]
 
 
-def lugar(coords, categ):
+def lugar(coords, categ, radio):
     return list(places.find(
         {"category": categ, "geometry": {"$near": {
             "$geometry": {"type": "Point",
                           "coordinates": coords
-                          }, "$maxDistance": 500}}}, {"name": 1, "longitude": 1, "latitude": 1, "category": 1}
+                          }, "$maxDistance": radio}}}, {"name": 1, "longitude": 1, "latitude": 1, "category": 1}
     ))
 
 
 def sum_gym(coords):
-    result = coords, len(lugar(coords, "gym"))
-    if result[1] > 2:
-        r = homes.find_one({"latitude": result[0][0], "longitude": result[0][1]})
+    result = coords, len(lugar(coords, "gym", 500))
+    if result[1] < 10:
+        r = homes.find_one({"latitude": result[0][0], "longitude": result[0][1]}, {"_id": 0, "province": 0, "municipality": 0, "geometry": 0})
+
         return r
     else:
         pass
 
 
 def sum_rest(coords):
-    result = coords, len(lugar(coords, "restaurant"))
-    if result[1] > 15:
-        r = homes.find_one({"latitude": result[0][0], "longitude": result[0][1]})
-        return r
-    else:
-        pass
-
-
-def sum_night(coords):
-    result = coords, len(lugar(coords, "nightlife"))
-    if result[1] > 15:
-        r = homes.find_one({"latitude": result[0][0], "longitude": result[0][1]})
+    result = coords, len(lugar(coords, "restaurant_nightlife", 500))
+    if result[1] > 7:
+        r = homes.find_one({"latitude": result[0][0], "longitude": result[0][1]}, {"_id": 0, "province": 0, "municipality": 0, "geometry": 0})
         return r
     else:
         pass
 
 
 def sum_superm(coords):
-    result = coords, len(lugar(coords, "supermarket"))
-    if result[1] > 2:
-        r = homes.find_one({"latitude": result[0][0], "longitude": result[0][1]})
-        return r
-    else:
-        pass
-
-
-def sum_store(coords):
-    result = coords, len(lugar(coords, "clothing_store"))
-    if result[1] > 10:
-        r = homes.find_one({"latitude": result[0][0], "longitude": result[0][1]})
+    result = coords, len(lugar(coords, "supermarket", 500))
+    if 1 <= result[1] <= 10:
+        r = homes.find_one({"latitude": result[0][0], "longitude": result[0][1]}, {"_id": 0, "province": 0, "municipality": 0, "geometry": 0})
         return r
     else:
         pass
 
 
 def sum_medical(coords):
-    result = coords, len(lugar(coords, "medical_centre"))
-    if result[1] > 2:
-        r = homes.find_one({"latitude": result[0][0], "longitude": result[0][1]})
+    result = coords, len(lugar(coords, "medical_centre", 5000))
+    if result[1] == 1:
+        r = homes.find_one({"latitude": result[0][0], "longitude": result[0][1]}, {"_id": 0, "province": 0, "municipality": 0, "geometry": 0})
         return r
     else:
         pass
 
 
 def sum_transport(coords):
-    result = coords, len(lugar(coords, "transport"))
-    if result[1] > 2:
-        r = homes.find_one({"latitude": result[0][0], "longitude": result[0][1]})
+    result = coords, len(lugar(coords, "transport", 500))
+    if 1 <= result[1] <= 10:
+        r = homes.find_one({"latitude": result[0][0], "longitude": result[0][1]}, {"_id": 0, "province": 0, "municipality": 0, "geometry": 0})
         return r
     else:
         pass
 
 
 def sum_ent(coords):
-    result = coords, len(lugar(coords, "general_entertainment"))
-    if result[1] > 15:
-        r = homes.find_one({"latitude": result[0][0], "longitude": result[0][1]})
+    result = coords, len(lugar(coords, "general_entertainment", 1000))
+    if result[1] > 5:
+        r = homes.find_one({"latitude": result[0][0], "longitude": result[0][1]}, {"_id": 0, "province": 0, "municipality": 0, "geometry": 0})
         return r
     else:
         pass
 
 
 def sum_pharmacy(coords):
-    result = coords, len(lugar(coords, "pharmacy"))
-    if result[1] > 2:
-        r = homes.find_one({"latitude": result[0][0], "longitude": result[0][1]})
+    result = coords, len(lugar(coords, "pharmacy", 500))
+    if result[1] == 1:
+        r = homes.find_one({"latitude": result[0][0], "longitude": result[0][1]}, {"_id": 0, "province": 0, "municipality": 0, "geometry": 0})
         return r
     else:
         pass
 
 
 def sum_parks(coords):
-    result = coords, len(lugar(coords, "park"))
-    if result[1] > 2:
-        r = homes.find_one({"latitude": result[0][0], "longitude": result[0][1]})
+    result = coords, len(lugar(coords, "park", 1000))
+    if 1 <= result[1] <= 5:
+        r = homes.find_one({"latitude": result[0][0], "longitude": result[0][1]}, {"_id": 0, "province": 0, "municipality": 0, "geometry": 0})
         return r
     else:
         pass
 
+
 def sum_school(coords):
-    result = coords, len(lugar(coords, "school"))
-    if result[1] > 5:
-        r = homes.find_one({"latitude": result[0][0], "longitude": result[0][1]})
+    result = coords, len(lugar(coords, "school", 500))
+    if 1 <= result[1] <= 5:
+        r = homes.find_one({"latitude": result[0][0], "longitude": result[0][1]}, {"_id": 0, "province": 0, "municipality": 0, "geometry": 0})
         return r
     else:
         pass
 
 
 def category(choice):
-
     if choice == "Supermarkets":
         return list(filter(None, map(sum_superm, coordinates)))
 
     elif choice == "Gyms":
         return list(filter(None, map(sum_gym, coordinates)))
-
-    elif choice == "Clothing store":
-        return list(filter(None, map(sum_store, coordinates)))
-
-    elif choice == "Nightlife":
-        return list(filter(None, map(sum_night, coordinates)))
 
     elif choice == "Transport":
         return list(filter(None, map(sum_transport, coordinates)))
@@ -143,7 +119,7 @@ def category(choice):
     elif choice == "Parks":
         return list(filter(None, map(sum_parks, coordinates)))
 
-    elif choice == "Restaurants":
+    elif choice == "Restaurants nd Nightlife":
         return list(filter(None, map(sum_rest, coordinates)))
 
     elif choice == "Pharmacy":
@@ -156,7 +132,7 @@ def category(choice):
         return list(filter(None, map(sum_school, coordinates)))
 
 
-def final(cat_1, cat_2, cat_3) -> object:
+def final(district, cat_1, cat_2, cat_3, proptype):
     lista = []
 
     lista.append(category(cat_1))
@@ -169,4 +145,6 @@ def final(cat_1, cat_2, cat_3) -> object:
 
     result = pd.concat([x, y, z])
 
-    return result[result.groupby('latitude').latitude.transform('count') > 1]
+    result = result[(result["district"] == district) & (result["propertyType"]==proptype)]
+
+    return result[result.groupby('latitude').latitude.transform('count') > 1].drop_duplicates(subset="latitude",keep='first')
